0459721c3dd6ac6ac26b3c4b3144b9a5a8c3f157
tydev-new <tydev2025@gmail.com>
2025-05-13 12:58:44 -0700

    Delete tests/system-auto/sh_counter.sh

--- tests/system-auto/sh_counter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-counter=0
-while [ $counter -lt 6000 ]; do
-    ts=$(date "+%Y-%m-%d %H:%M:%S")
-    #echo "[$ts] Counter: $counter"
-    echo "[$ts] $counter" >> sh_counter_output.txt
-    counter=$((counter+1))
-    sleep 3
-done
